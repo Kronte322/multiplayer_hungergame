03417d32d8db11e9ab8a8d8eb5cbeaec0456c497
Kronte322 <drachev.da@phystech.edu>
2023-05-07 21:48:35 +0300

    final for review

--- src/front/Window.py
+++ src/front/Window.py
@@ -6,7 +6,7 @@ class Window:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(CAPTION)
-        self.display = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.display = pygame.display.set_mode(SIZE_OF_DISPLAY)
 
     def GetDisplay(self):
         return self.display
