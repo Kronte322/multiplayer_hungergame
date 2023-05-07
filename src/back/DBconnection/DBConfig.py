064f4c0063a0a115adac69d27428fe91c492dee4
Kronte322 <drachev.da@phystech.edu>
2023-05-07 22:54:53 +0300

    final for review

--- src/back/DBconnection/DBConfig.py
+++ src/back/DBconnection/DBConfig.py
@@ -1,6 +1,6 @@
 # db connection config
-DB_NAME = 'postgres'
-DB_IP_ADDRESS = '192.168.0.108'
+DB_NAME = ''
+DB_IP_ADDRESS = ''
 DB_PORT = 5432
-DB_USER = 'postgres'
-DB_USER_PASSWORD = 'antisocialplayer'
+DB_USER = ''
+DB_USER_PASSWORD = ''
